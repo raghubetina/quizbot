8f0b292917d2afec632e7ce14ad26be4c6acbd95
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2024-11-20 23:31:49 +0000

    Set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  # get("/", { :controller => "quizzes", :action => "index" })
+  
+  root to: "quizzes#index"
+
   # Routes for the Message resource:
 
   # CREATE
